c725c43eb6e271e254a923d34251e22a4c0ab7ec
Zehao <632607887@qq.com>
2015-03-26 15:05:17 +0800

    添加说明

--- dev/js/app/home.js
+++ dev/js/app/home.js
@@ -1,13 +1,22 @@
 requirejs(['jquery', 'common/lazyload.init', 'common/telinput.init'], function($) {
 	console.log('Module home loaded.');
 
-	var $modal = $('#myModal');
 
-	$('#btn-ok').on('click', function() {
-		$modal.modal('hide');
-	});
+	(function($modal) {
+		$('#btn-ok').on('click', function() {
+			$modal.modal('hide');
+		});
 
-	$modal.on('hidden.bs.modal', function() {
-		alert('Thanks!');
-	});
+		$modal.on('hidden.bs.modal', function() {
+			alert('Thanks!');
+		});
+	}($('#myModal')));
+
+	(function($affix) {
+		$affix.affix({
+			offset: {
+				top: 730
+			}
+		});
+	}($('#affix')));
 });
